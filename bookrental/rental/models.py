77f05ebd76b82f6fddbbe6fff4e7838360efe4ee
rahul24rajput <rrajput@QT-INNO-MAC0106.local>
2024-06-08 23:35:02 +0530

    Adding book models

--- bookrental/rental/models.py
+++ bookrental/rental/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.utils import timezone
+from datetime import timedelta
 
-# Create your models here.
+class Student(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
+    page_count = models.IntegerField()
+
+    def __str__(self):
+        return self.title
